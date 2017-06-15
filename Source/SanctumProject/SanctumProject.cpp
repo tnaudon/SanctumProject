d6dbbcf04b9b4b31e643cb7f96fcea279719b10f
Tristan NAUDON <t.naudon@student.isartdigital.com>
2017-06-15 11:51:07 +0200

    add unreal project files

---
+++ Source/SanctumProject/SanctumProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SanctumProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SanctumProject, "SanctumProject" );
---
+++ Source/SanctumProject/SanctumProjectGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SanctumProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SANCTUMPROJECT_API ASanctumProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
